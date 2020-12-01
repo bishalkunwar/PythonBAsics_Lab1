c0fcd4530b9b54b6d567c76ae885073755bba1d9
bishalkunwar <72371055+bishalkunwar@users.noreply.github.com>
2020-12-01 21:41:06 +0545

    Add files via upload

---
+++ FindGreatestNumber.py
@@ -0,0 +1,18 @@
+#Write the python program to find the greatest number among the three numbers.
+
+#Solution:
+def greatest(num1,num2,num3):
+    if num1 >= num2 and num1 >= num3:
+        return num1
+    elif num2 >= num3 and num2 >= num1:
+        return num2
+    else:
+        return num3
+
+
+num1 = int(input("Enter the first number:"))
+num2 = int(input("Enter the second number:"))
+num3 = int(input("Enter the third number:"))
+
+largest = greatest(num1, num2, num3)
+print(largest,"is the largest number among the given input numbers.")
